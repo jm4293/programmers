e7cf04692e7f6b9423df90aaa4499e2c04ed30b0
peek <dlwoals4293@gmail.com>
2025-09-15 21:02:40 +0900

    [level 2] Title: 전화번호 목록, Time: 241.22 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,14 @@
+function solution(phone_book) {
+  const set = new Set(phone_book);
+
+  for (const phone of phone_book) {
+    for (let i = 1; i < phone.length; i++) {
+      const prefix = phone.substring(0, i);
+      if (set.has(prefix)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
