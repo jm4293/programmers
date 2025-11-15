eaa5264697fddb92d7bf067ced177efb73efeadd
PEEK <dlwoals4293@gmail.com>
2025-11-15 19:45:20 +0900

    [level 2] Title: 전화번호 목록, Time: 271.21 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,10 @@
 function solution(phone_book) {
-  // 정렬하면 접두어 관계가 있는 번호들이 인접하게 됨
-  phone_book.sort();
+  const set = new Set(phone_book);
   
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    // 다음 번호가 현재 번호로 시작하는지 확인
-    if (phone_book[i + 1].startsWith(phone_book[i])) {
-      return false;
+  for (const phone of phone_book) {
+    for (let i = 1; i < phone.length; i++) {
+      const prefix = phone.substring(0, i);
+      if (set.has(prefix)) return false;
     }
   }
   
