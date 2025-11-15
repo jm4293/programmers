ea844c8ba14cd5cb53d1a75f29e2bcda9ea4928b
PEEK <dlwoals4293@gmail.com>
2025-11-15 23:22:46 +0900

    [level 2] Title: 기능개발, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,23 +1,23 @@
 function solution(progresses, speeds) {
-  const days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-
-  let maxDay = days[0];
-
-  return days.reduce(
-    (acc, day, index, arr) => {
-      if (index === 0) {
-        return acc;
-      }
-
-      if (day <= maxDay) {
-        acc[acc.length - 1]++;
-      } else {
-        maxDay = day;
-        acc.push(1);
-      }
-
-      return acc;
-    },
-    [1],
+  // 1. 각 기능의 완료 일수 계산
+  const days = progresses.map((progress, index) => 
+    Math.ceil((100 - progress) / speeds[index])
   );
+  
+  // 2. 배포 그룹핑
+  const result = [];
+  let maxDay = 0;
+  
+  days.forEach((day) => {
+    if (day > maxDay) {
+      // 새로운 배포 그룹 시작
+      result.push(1);
+      maxDay = day;
+    } else {
+      // 같은 배포 그룹에 추가
+      result[result.length - 1]++;
+    }
+  });
+  
+  return result;
 }
