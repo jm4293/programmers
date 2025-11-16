bc26dda90b1f144a69c3be8486e66d1cfc8a198d
PEEK <dlwoals4293@gmail.com>
2025-11-16 19:39:06 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,12 +1,12 @@
 function solution(array, commands) {
-  var answer = [];
+  const result = [];
 
   commands.forEach((command) => {
     const [i, j, k] = command;
-    const sliced = array.slice(i - 1, j);
-    const sorted = sliced.sort((a, b) => a - b);
-    answer.push(sorted[k - 1]);
+    const slicedArray = array.slice(i - 1, j);
+    const sortedArray = slicedArray.sort((a, b) => a - b);
+    result.push(sortedArray[k - 1]);
   });
 
-  return answer;
+  return result;
 }
