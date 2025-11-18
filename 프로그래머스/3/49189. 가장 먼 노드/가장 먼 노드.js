319f156902ed22695e950f4ea0aea856cd41c8b0
PEEK <dlwoals4293@gmail.com>
2025-11-18 11:54:03 +0900

    [level 3] Title: 가장 먼 노드, Time: 55.37 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,27 +1,36 @@
 function solution(n, edge) {
-  const graph = Array.from({ length: n + 1 }, () => []);
+  const graph = new Map();
 
-  for (const [a, b] of edge) {
-    graph[a].push(b);
-    graph[b].push(a);
-  }
+  edge.forEach(([a, b]) => {
+    if (!graph.has(a)) {
+      graph.set(a, []);
+    }
+    if (!graph.has(b)) {
+      graph.set(b, []);
+    }
 
-  const distances = Array(n + 1).fill(Infinity);
-  distances[1] = 0;
+    graph.get(a).push(b);
+    graph.get(b).push(a);
+  });
 
+  const visited = new Array(n + 1).fill(false);
+  const distance = new Array(n + 1).fill(0);
+  let maxDistance = 0;
   const queue = [1];
+  visited[1] = true;
+  distance[1] = 0;
 
-  while (queue.length) {
-    const node = queue.shift();
-
-    for (const neighbor of graph[node]) {
-      if (distances[neighbor] === Infinity) {
-        distances[neighbor] = distances[node] + 1;
+  while (queue.length > 0) {
+    const current = queue.shift();
+    graph.get(current).forEach((neighbor) => {
+      if (!visited[neighbor]) {
+        visited[neighbor] = true;
+        distance[neighbor] = distance[current] + 1;
         queue.push(neighbor);
       }
-    }
+      maxDistance = Math.max(maxDistance, distance[neighbor]);
+    });
   }
 
-  const maxDistance = Math.max(...distances.filter((d) => d !== Infinity));
-  return distances.filter((d) => d === maxDistance).length;
+  return distance.filter((dist) => dist === maxDistance).length;
 }
