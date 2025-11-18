df5202d6b7aca4a1d723d79eb9bc912a44fb5e02
PEEK <dlwoals4293@gmail.com>
2025-11-18 15:39:56 +0900

    [level 2] Title: 더 맵게, Time: 735.77 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,84 +1,98 @@
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-  push(value) {
-    this.heap.push(value);
-    this._bubbleUp();
-  }
-  pop() {
-    if (this.heap.length === 1) {
-      return this.heap.pop();
-    }
+function createMinHeap() {
+  const a = [];
 
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this._bubbleDown();
+  function up(i) {
+    while (i > 0) {
+      const p = Math.floor((i - 1) / 2);
 
-    return min;
-  }
-  peek() {
-    return this.heap[0];
-  }
-  size() {
-    return this.heap.length;
-  }
-  _bubbleUp() {
-    let idx = this.heap.length - 1;
-
-    while (idx > 0) {
-      let parent = Math.floor((idx - 1) / 2);
-
-      if (this.heap[parent] <= this.heap[idx]) {
+      if (a[p] <= a[i]) {
         break;
       }
 
-      [this.heap[parent], this.heap[idx]] = [this.heap[idx], this.heap[parent]];
-      idx = parent;
+      [a[p], a[i]] = [a[i], a[p]];
+      i = p;
     }
   }
-  _bubbleDown() {
-    let idx = 0;
-    const length = this.heap.length;
+
+  function down(i) {
+    const n = a.length;
 
     while (true) {
-      let left = 2 * idx + 1;
-      let right = 2 * idx + 2;
-      let smallest = idx;
+      let s = i;
+      let l = i * 2 + 1;
+      let r = l + 1;
 
-      if (left < length && this.heap[left] < this.heap[smallest]) {
-        smallest = left;
+      if (l < n && a[l] < a[s]) {
+        s = l;
       }
 
-      if (right < length && this.heap[right] < this.heap[smallest]) {
-        smallest = right;
+      if (r < n && a[r] < a[s]) {
+        s = r;
       }
 
-      if (smallest === idx) {
+      if (s === i) {
         break;
       }
 
-      [this.heap[smallest], this.heap[idx]] = [this.heap[idx], this.heap[smallest]];
-      idx = smallest;
+      [a[i], a[s]] = [a[s], a[i]];
+      i = s;
     }
   }
+
+  return {
+    size() {
+      return a.length;
+    },
+    peek() {
+      return a[0];
+    },
+    // 디버깅용: 배열 전체 확인
+    debug() {
+      return [...a];
+    },
+    push(x) {
+      a.push(x);
+      up(a.length - 1);
+    },
+    pop() {
+      if (!a.length) {
+        return undefined;
+      }
+
+      const top = a[0];
+      const last = a.pop();
+
+      if (a.length) {
+        a[0] = last;
+        down(0);
+      }
+
+      return top;
+    },
+  };
 }
 
 function solution(scoville, K) {
-  const heap = new MinHeap();
+  const heap = createMinHeap();
 
-  for (const s of scoville) {
-    heap.push(s);
-  }
+  scoville.forEach((x) => heap.push(x));
 
-  let answer = 0;
+  let count = 0;
 
-  while (heap.size() > 1 && heap.peek() < K) {
+  while (heap.size()) {
+    if (heap.peek() >= K) {
+      return count;
+    }
+
     const first = heap.pop();
+
+    if (heap.size() === 0) {
+      return -1;
+    }
+
     const second = heap.pop();
     heap.push(first + second * 2);
-    answer++;
+    count++;
   }
-  
-  return heap.peek() >= K ? answer : -1;
+  return -1;
 }
