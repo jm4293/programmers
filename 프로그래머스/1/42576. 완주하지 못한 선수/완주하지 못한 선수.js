8875eefe63db7ac3e8d39ec054acf69de7de748b
PEEK <dlwoals4293@gmail.com>
2025-12-28 22:32:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 51.63 ms, Memory: 64.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,7 +2,6 @@ function solution(participant, completion) {
   const participantMap = new Map();
 
   participant.forEach((name) => participantMap.set(name, (participantMap.get(name) || 0) + 1));
-
   completion.forEach((name) => participantMap.set(name, participantMap.get(name) - 1));
 
   for (const [name, count] of participantMap) {
