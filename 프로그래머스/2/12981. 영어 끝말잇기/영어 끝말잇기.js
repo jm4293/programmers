1898913c2ceb9e3268518af694d0b09851535102
PEEK <dlwoals4293@gmail.com>
2025-12-29 14:05:01 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,11 +1,11 @@
 function solution(n, words) {
   let usedWords = new Set();
-  let lastChar = '';
+  let lastChar = words[0][0];
 
   for (let i = 0; i < words.length; i++) {
     const word = words[i];
 
-    if (usedWords.has(word) || (lastChar && word[0] !== lastChar)) {
+    if (usedWords.has(word) || word[0] !== lastChar) {
       const person = (i % n) + 1;
       const turn = Math.floor(i / n) + 1;
 
