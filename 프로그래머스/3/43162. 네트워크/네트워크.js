bb7b64ba79d8112a69aee4101be71585931936f0
PEEK <dlwoals4293@gmail.com>
2025-12-29 19:49:10 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,26 +1,22 @@
 function solution(n, computers) {
-  const visited = new Array(n).fill(false);
-  let networkCount = 0;
+  let visited = new Array(n).fill(false);
+  let answer = 0;
 
-  for (let i = 0; i < n; i++) {
-    if (!visited[i]) {
-      networkCount++;
-
-      const queue = [i];
-      visited[i] = true;
-
-      while (queue.length > 0) {
-        const current = queue.shift();
-
-        for (let j = 0; j < n; j++) {
-          if (computers[current][j] === 1 && !visited[j]) {
-            visited[j] = true;
-            queue.push(j);
-          }
-        }
+  function dfs(node) {
+    visited[node] = true;
+    for (let i = 0; i < n; i++) {
+      if (computers[node][i] === 1 && !visited[i]) {
+        dfs(i);
       }
     }
   }
 
-  return networkCount;
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      answer++;
+    }
+  }
+
+  return answer;
 }
