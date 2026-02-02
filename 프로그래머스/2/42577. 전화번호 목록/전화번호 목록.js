3d064e55d611fcfbdfc92d93a40143bb0fa03f79
jm4293 <dlwoals4293@gmail.com>
2026-02-02 14:23:02 +0900

    [level 2] Title: 전화번호 목록, Time: 164.24 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,11 @@
 function solution(phone_book) {
   phone_book.sort();
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    if (phone_book[i + 1].startsWith(phone_book[i])) {
+
+  for (let [i, phone] of phone_book.entries()) {
+    if (i < phone_book.length - 1 && phone_book[i + 1].startsWith(phone)) {
       return false;
     }
   }
+
   return true;
 }
