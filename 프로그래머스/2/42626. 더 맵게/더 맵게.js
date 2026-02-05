973a05b4b1810f346a4e1a779f5f71772338f6ee
jm4293 <dlwoals4293@gmail.com>
2026-02-05 13:38:29 +0900

    [level 2] Title: 더 맵게, Time: 661.71 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,98 +1,66 @@
-function createMinHeap() {
-  const a = [];
-
-  function up(i) {
-    while (i > 0) {
-      const p = Math.floor((i - 1) / 2);
-
-      if (a[p] <= a[i]) {
-        break;
-      }
-
-      [a[p], a[i]] = [a[i], a[p]];
-      i = p;
-    }
-  }
-
-  function down(i) {
-    const n = a.length;
-
-    while (true) {
-      let s = i;
-      let l = i * 2 + 1;
-      let r = l + 1;
-
-      if (l < n && a[l] < a[s]) {
-        s = l;
-      }
-
-      if (r < n && a[r] < a[s]) {
-        s = r;
-      }
-
-      if (s === i) {
-        break;
-      }
-
-      [a[i], a[s]] = [a[s], a[i]];
-      i = s;
-    }
-  }
-
-  return {
-    size() {
-      return a.length;
-    },
-    peek() {
-      return a[0];
-    },
-    // 디버깅용: 배열 전체 확인
-    debug() {
-      return [...a];
-    },
-    push(x) {
-      a.push(x);
-      up(a.length - 1);
-    },
-    pop() {
-      if (!a.length) {
-        return undefined;
-      }
-
-      const top = a[0];
-      const last = a.pop();
-
-      if (a.length) {
-        a[0] = last;
-        down(0);
-      }
-
-      return top;
-    },
-  };
-}
-
 function solution(scoville, K) {
-  const heap = createMinHeap();
+  const heap = [];
 
-  scoville.forEach((x) => heap.push(x));
+  const push = (value) => {
+    heap.push(value);
+    let index = heap.length - 1;
+    while (index > 0) {
+      const parentIdx = Math.floor((index - 1) / 2);
+      if (heap[index] >= heap[parentIdx]) break;
+      [heap[index], heap[parentIdx]] = [heap[parentIdx], heap[index]];
+      index = parentIdx;
+    }
+  };
+
+  const pop = () => {
+    if (heap.length === 0) return null;
+    if (heap.length === 1) return heap.pop();
+
+    const min = heap[0];
+    heap[0] = heap.pop();
+
+    let index = 0;
+    while (true) {
+      const leftIdx = 2 * index + 1;
+      const rightIdx = 2 * index + 2;
+      let smallestIdx = index;
+
+      if (leftIdx < heap.length && heap[leftIdx] < heap[smallestIdx]) {
+        smallestIdx = leftIdx;
+      }
+      if (rightIdx < heap.length && heap[rightIdx] < heap[smallestIdx]) {
+        smallestIdx = rightIdx;
+      }
+
+      if (smallestIdx === index) break;
+
+      [heap[index], heap[smallestIdx]] = [heap[smallestIdx], heap[index]];
+      index = smallestIdx;
+    }
+    return min;
+  };
+
+  // 1. 초기 힙 구성
+  for (const s of scoville) {
+    push(s);
+  }
 
   let count = 0;
 
-  while (heap.size()) {
-    if (heap.peek() >= K) {
-      return count;
-    }
+  // 2. 가장 작은 값이 K 이상이 될 때까지 반복
+  while (heap.length >= 2 && heap[0] < K) {
+    const first = pop(); // 가장 안 매운 음식
+    const second = pop(); // 두 번째로 안 매운 음식
 
-    const first = heap.pop();
-
-    if (heap.size() === 0) {
-      return -1;
-    }
-
-    const second = heap.pop();
-    heap.push(first + second * 2);
+    const mixed = first + second * 2;
+    push(mixed);
     count++;
   }
-  return -1;
+
+  // 3. 반복이 끝난 후, 가장 작은 값이 K 이상인지 확인
+  if (heap[0] < K) {
+    return -1;
+  }
+
+  return count;
 }
