eda5307c2776748cfb41d628c99ffe960985b8f5
jm4293 <dlwoals4293@gmail.com>
2026-02-11 21:45:32 +0900

    [level 2] Title: 소수 찾기, Time: 25.60 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,65 +1,35 @@
-function isPrime(n) {
-  if (!Number.isInteger(n) || n < 2) {
-    return false;
-  }
-
-  const limit = Math.floor(Math.sqrt(n));
-
-  for (let i = 2; i <= limit; i++) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function permutations(arr, k) {
-  const res = [];
-  const used = Array(arr.length).fill(false);
-  const path = [];
-
-  function dfs() {
-    if (path.length === k) {
-      res.push([...path]);
-      return;
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-      if (used[i]) {
-        continue;
-      }
-
-      used[i] = true;
-      path.push(arr[i]);
-      dfs();
-      path.pop();
-      used[i] = false;
-    }
-  }
-
-  dfs();
-
-  return res;
-}
-
 function solution(numbers) {
-  const digits = numbers.split('');
-  const made = new Set();
+  const permutations = new Set();
 
-  for (let k = 1; k <= digits.length; k++) {
-    for (const p of permutations(digits, k)) {
-      const num = Number(p.join(''));
-      made.add(num);
+  const generatePermutations = (current, remaining) => {
+    if (current.length > 0) {
+      permutations.add(parseInt(current, 10));
     }
-  }
 
-  let count = 0;
-  made.forEach((num) => {
+    for (let i = 0; i < remaining.length; i++) {
+      generatePermutations(
+        current + remaining[i],
+        remaining.slice(0, i) + remaining.slice(i + 1),
+      );
+    }
+  };
+
+  generatePermutations("", numbers);
+
+  const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+
+  let primeCount = 0;
+  permutations.forEach((num) => {
     if (isPrime(num)) {
-      count++;
+      primeCount++;
     }
   });
 
-  return count;
+  return primeCount;
 }
