d6485f7876237a8ee3353e28da3ed25edddf2fe2
jm4293 <dlwoals4293@gmail.com>
2026-02-11 23:44:37 +0900

    [level 2] Title: 피로도, Time: 5.38 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,19 +1,18 @@
 function solution(k, dungeons) {
   let maxCount = 0;
-  const visited = Array(dungeons.length).fill(false);
 
-  function dfs(currentK, count) {
+  function dfs(remainingFatigue, visited, count) {
     maxCount = Math.max(maxCount, count);
 
     for (let i = 0; i < dungeons.length; i++) {
-      if (!visited[i] && currentK >= dungeons[i][0]) {
+      if (!visited[i] && remainingFatigue >= dungeons[i][0]) {
         visited[i] = true;
-        dfs(currentK - dungeons[i][1], count + 1);
+        dfs(remainingFatigue - dungeons[i][1], visited, count + 1);
         visited[i] = false;
       }
     }
   }
 
-  dfs(k, 0);
+  dfs(k, Array(dungeons.length).fill(false), 0);
   return maxCount;
 }
