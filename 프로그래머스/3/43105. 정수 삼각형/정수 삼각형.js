a749161655cfa318c4dd7359a4a57a08c180a257
jm4293 <dlwoals4293@gmail.com>
2026-02-14 18:51:45 +0900

    [level 3] Title: 정수 삼각형, Time: 4.33 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,6 +1,6 @@
 function solution(triangle) {
   const dp = triangle.map(row => [...row]);
-    
+
   for (let i = dp.length - 2; i >= 0; i--) {
     for (let j = 0; j < dp[i].length; j++) {
       dp[i][j] += Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
