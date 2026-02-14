f84d7a75bd201e2b8ef905b67feffe7c0f384ecc
jm4293 <dlwoals4293@gmail.com>
2026-02-14 18:24:47 +0900

    [level 3] Title: N으로 표현, Time: 41.85 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,30 +1,25 @@
 function solution(N, number) {
-  if (N === number) return 1;
-
-  // dp[i] = N을 i개 사용해서 만들 수 있는 숫자들의 집합
   const dp = Array.from({ length: 9 }, () => new Set());
 
-  // N을 i개 연속으로 붙인 수 (5, 55, 555, ...)
   for (let i = 1; i <= 8; i++) {
     dp[i].add(Number(String(N).repeat(i)));
-  }
 
-  // N을 i개 사용해서 만들 수 있는 모든 경우
-  for (let i = 1; i <= 8; i++) {
     for (let j = 1; j < i; j++) {
-      // j개 사용한 수와 (i-j)개 사용한 수를 사칙연산
-      for (const num1 of dp[j]) {
-        for (const num2 of dp[i - j]) {
-          dp[i].add(num1 + num2);
-          dp[i].add(num1 - num2);
-          dp[i].add(num1 * num2);
-          if (num2 !== 0) dp[i].add(Math.floor(num1 / num2));
+      for (const a of dp[j]) {
+        for (const b of dp[i - j]) {
+          dp[i].add(a + b);
+          dp[i].add(a - b);
+          dp[i].add(a * b);
+          if (b !== 0) {
+            dp[i].add(Math.floor(a / b));
+          }
         }
       }
     }
 
-    // number를 찾으면 즉시 반환
-    if (dp[i].has(number)) return i;
+    if (dp[i].has(number)) {
+      return i;
+    }
   }
 
   return -1;
