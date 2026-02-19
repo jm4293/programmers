915a91b21fd7389ef52decf48453b26ad6e7577c
jm4293 <dlwoals4293@gmail.com>
2026-02-19 14:20:59 +0900

    [level 1] Title: 유연근무제, Time: 3.93 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
 
 ### 성능 요약
 
-메모리: 33.8 MB, 시간: 0.67 ms
+메모리: 35.8 MB, 시간: 3.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 15일 00:28:58
+2026년 02월 19일 14:20:57
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.js
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -1,51 +1,37 @@
+function addTenMinutes(time) {
+  let hour = Math.floor(time / 100);
+  let min = time % 100;
+  min += 10;
+
+  if (min >= 60) {
+    hour += 1;
+    min -= 60;
+  }
+
+  return hour * 100 + min;
+}
+
 function solution(schedules, timelogs, startday) {
   let answer = 0;
 
-  // 출근 인정 시각 계산 함수 (희망 시각 + 10분)
-  function getDeadlineTime(schedule) {
-    const hour = Math.floor(schedule / 100);
-    const minute = schedule % 100;
-    
-    let newMinute = minute + 10;
-    let newHour = hour;
-    
-    if (newMinute >= 60) {
-      newHour += 1;
-      newMinute -= 60;
-    }
-    
-    return newHour * 100 + newMinute;
-  }
-
-  // 요일이 평일인지 확인 (1=월, 2=화, 3=수, 4=목, 5=금, 6=토, 7=일)
-  function isWeekday(day) {
-    return day >= 1 && day <= 5;
-  }
-
   for (let i = 0; i < schedules.length; i++) {
-    const schedule = schedules[i];
-    const deadline = getDeadlineTime(schedule);
-    const log = timelogs[i];
-    
-    let allOnTime = true;
-    
-    // 7일 동안 체크
-    for (let day = 0; day < 7; day++) {
-      const currentDay = ((startday - 1 + day) % 7) + 1; // 1~7로 변환
-      
-      // 평일만 체크
-      if (isWeekday(currentDay)) {
-        const arrivalTime = log[day];
-        
-        // 출근 인정 시각보다 늦으면 지각
-        if (arrivalTime > deadline) {
-          allOnTime = false;
-          break;
-        }
+    let isLate = false;
+
+    for (let j = 0; j < timelogs[i].length; j++) {
+      let dayOfWeek = (startday - 1 + j) % 7;
+
+      if (dayOfWeek === 5 || dayOfWeek === 6) {
+        continue;
+      }
+
+      let limit = addTenMinutes(schedules[i]);
+      if (timelogs[i][j] > limit) {
+        isLate = true;
+        break;
       }
     }
-    
-    if (allOnTime) {
+
+    if (!isLate) {
       answer++;
     }
   }
