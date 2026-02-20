8edde7464748d79ae4fde58b96432b83d97c0ea8
jm4293 <dlwoals4293@gmail.com>
2026-02-20 14:14:04 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,25 +1,17 @@
 function solution(n, w, num) {
-  // num의 (row, col) 좌표 구하기
   const row = Math.floor((num - 1) / w);
   const idx = (num - 1) % w;
   const col = row % 2 === 0 ? idx : w - 1 - idx;
+  const height = Math.ceil(n / w);
+  let answer = 0;
 
-  let count = 0;
+  for (let i = row + 1; i < height; i++) {
+    const boxNum = i % 2 === 0 ? i * w + col + 1 : i * w + (w - 1 - col) + 1;
 
-  // 전체 층 수
-  const maxRow = Math.ceil(n / w);
-
-  // row부터 위로 검사
-  for (let r = row; r < maxRow; r++) {
-    let boxNum;
-
-    if (r % 2 === 0) {
-      boxNum = r * w + col + 1;
-    } else {
-      boxNum = r * w + (w - 1 - col) + 1;
+    if (boxNum <= n) {
+      answer++;
     }
-    if (boxNum > n) break;
-    count++;
   }
-  return count;
+
+  return answer + 1;
 }
