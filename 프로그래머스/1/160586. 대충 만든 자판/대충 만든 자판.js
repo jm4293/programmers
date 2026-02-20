eaf980094fe08e46558df5a48478e8a9f1ba74de
jm4293 <dlwoals4293@gmail.com>
2026-02-20 20:52:40 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,30 +1,27 @@
 function solution(keymap, targets) {
-  const keyObj = {};
+  const map = new Map();
 
-  keymap.forEach((key) => {
-    key.split('').forEach((char, index) => {
-      if (!keyObj[char] || keyObj[char] > index + 1) {
-        keyObj[char] = index + 1;
+  keymap.forEach((str) => {
+    str.split("").forEach((char, index) => {
+      if (map.has(char)) {
+        const prev = map.get(char);
+        map.set(char, Math.min(prev, index + 1));
+      } else {
+        map.set(char, index + 1);
       }
     });
   });
 
-  return targets.map((target) => {
-    let total = 0;
-
-    for (const char of target.split('')) {
-      if (keyObj[char]) {
-        total += keyObj[char];
-      } else {
-        total = -1;
-        break;
+  const answer = targets.map((target) => {
+    let count = 0;
+    for (const char of target) {
+      if (!map.has(char)) {
+        return -1;
       }
+      count += map.get(char);
     }
-
-    if (total === 0) {
-      total = -1;
-    }
-
-    return total;
+    return count;
   });
+
+  return answer;
 }
