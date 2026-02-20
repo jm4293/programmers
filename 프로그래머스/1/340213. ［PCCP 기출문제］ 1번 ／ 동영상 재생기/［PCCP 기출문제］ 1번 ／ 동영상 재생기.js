3fba4981a842fcab4db723377a7c5dc84eab3425
jm4293 <dlwoals4293@gmail.com>
2026-02-20 15:05:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,54 +1,37 @@
 function solution(video_len, pos, op_start, op_end, commands) {
-  // "mm:ss" 형식을 초 단위로 변환
-  function timeToSeconds(timeStr) {
-    const [mm, ss] = timeStr.split(':').map(Number);
-    return mm * 60 + ss;
+  const toSec = (str) => {
+    const [m, s] = str.split(":").map(Number);
+    return m * 60 + s;
+  };
+
+  const toStr = (sec) => {
+    const m = String(Math.floor(sec / 60)).padStart(2, "0");
+    const s = String(sec % 60).padStart(2, "0");
+    return `${m}:${s}`;
+  };
+
+  let cur = toSec(pos);
+  const videoLen = toSec(video_len);
+  const opStart = toSec(op_start);
+  const opEnd = toSec(op_end);
+
+  if (cur >= opStart && cur <= opEnd) {
+    cur = opEnd;
   }
-  
-  // 초 단위를 "mm:ss" 형식으로 변환
-  function secondsToTime(seconds) {
-    const mm = Math.floor(seconds / 60);
-    const ss = seconds % 60;
-    return `${String(mm).padStart(2, '0')}:${String(ss).padStart(2, '0')}`;
-  }
-  
-  // 현재 위치를 초 단위로 변환
-  let currentSeconds = timeToSeconds(pos);
-  const videoLenSeconds = timeToSeconds(video_len);
-  const opStartSeconds = timeToSeconds(op_start);
-  const opEndSeconds = timeToSeconds(op_end);
-  
-  // 오프닝 구간 체크 함수
-  function checkOpening() {
-    if (currentSeconds >= opStartSeconds && currentSeconds <= opEndSeconds) {
-      currentSeconds = opEndSeconds;
+
+  for (const cmd of commands) {
+    if (cmd === "prev") {
+      cur = Math.max(0, cur - 10);
+    }
+
+    if (cmd === "next") {
+      cur = Math.min(videoLen, cur + 10);
+    }
+
+    if (cur >= opStart && cur <= opEnd) {
+      cur = opEnd;
     }
   }
-  
-  // 시작 위치가 오프닝 구간인지 체크
-  checkOpening();
-  
-  // 명령 처리
-  for (const command of commands) {
-    if (command === 'prev') {
-      // 10초 전으로 이동
-      if (currentSeconds < 10) {
-        currentSeconds = 0;
-      } else {
-        currentSeconds -= 10;
-      }
-    } else if (command === 'next') {
-      // 10초 후로 이동
-      if (currentSeconds + 10 > videoLenSeconds) {
-        currentSeconds = videoLenSeconds;
-      } else {
-        currentSeconds += 10;
-      }
-    }
-    
-    // 명령 실행 후 오프닝 구간 체크
-    checkOpening();
-  }
-  
-  return secondsToTime(currentSeconds);
+
+  return toStr(cur);
 }
