c62fca8954d45338e7dc96e6e49c048b92919073
jm4293 <dlwoals4293@gmail.com>
2026-02-20 16:28:57 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 13.22 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,51 +1,34 @@
 function solution(friends, gifts) {
-  // 각 친구별로 주고받은 선물의 수를 기록
-  let giftCounts = {};
-  friends.forEach((friend) => {
-    giftCounts[friend] = friends.reduce((acc, cur) => {
-      acc[cur] = { given: 0, received: 0 };
-      return acc;
-    }, {});
-  });
+  const idx = {};
+  friends.forEach((name, i) => (idx[name] = i));
+  const n = friends.length;
 
-  // 선물 기록 분석
-  gifts.forEach((el) => {
-    let [giver, receiver] = el.split(' ');
-    giftCounts[giver][receiver].given++;
-    giftCounts[receiver][giver].received++;
-  });
+  const table = Array.from({ length: n }, () => Array(n).fill(0));
+  for (const gift of gifts) {
+    const [giver, receiver] = gift.split(" ");
+    table[idx[giver]][idx[receiver]]++;
+  }
 
-  // 선물 지수 계산
-  let giftScores = {};
-  friends.forEach((el) => {
-    let totalGiven = 0;
-    let totalReceived = 0;
-
-    for (let other in giftCounts[el]) {
-      totalGiven += giftCounts[el][other].given;
-      totalReceived += giftCounts[el][other].received;
+  const score = Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      score[i] += table[i][j] - table[j][i];
     }
-    giftScores[el] = totalGiven - totalReceived;
-  });
+  }
 
-  // 다음 달 선물 예측
-  let nextMonthGifts = friends.map((el) => {
-    let giftsReceived = 0;
-
-    friends.forEach((el2) => {
-      if (el !== el2) {
-        if (giftCounts[el][el2].given > giftCounts[el][el2].received) {
-          giftsReceived++;
-        } else if (giftCounts[el][el2].given === giftCounts[el][el2].received) {
-          if (giftScores[el] > giftScores[el2]) {
-            giftsReceived++;
-          }
-        }
+  const next = Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (table[i][j] > table[j][i]) {
+        next[i]++;
+      } else if (table[i][j] < table[j][i]) {
+        next[j]++;
+      } else {
+        if (score[i] > score[j]) next[i]++;
+        else if (score[i] < score[j]) next[j]++;
       }
-    });
-    return giftsReceived;
-  });
+    }
+  }
 
-  // 가장 많은 선물을 받을 친구 찾기
-  return Math.max(...nextMonthGifts)
+  return Math.max(...next);
 }
