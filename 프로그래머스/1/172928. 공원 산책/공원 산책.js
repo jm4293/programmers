51b8d12a0587ba185ff64feb7246d3d944725a0a
jm4293 <dlwoals4293@gmail.com>
2026-02-20 18:43:18 +0900

    [level 1] Title: 공원 산책, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,42 +1,39 @@
 function solution(park, routes) {
-    const h = park.length;
-    const w = park[0].length;
+  const H = park.length;
+  const W = park[0].length;
+  const dir = { N: [-1, 0], S: [1, 0], W: [0, -1], E: [0, 1] };
 
-    // 시작 위치 찾기
-    const startPos = park.reduce((acc, row, i) => {
-        const j = row.indexOf('S');
-        return j !== -1 ? [i, j] : acc;
-    }, [0, 0]);
+  let r, c;
+  for (let i = 0; i < H; i++) {
+    const j = park[i].indexOf("S");
 
-    // 방향에 따른 이동 좌표 변화
-    const direction = {
-        'N': [-1, 0],
-        'S': [1, 0],
-        'W': [0, -1],
-        'E': [0, 1]
-    };
+    if (j !== -1) {
+      r = i;
+      c = j;
+      break;
+    }
+  }
 
-    // 명령을 처리하는 함수
-    const move = (pos, [op, n]) => {
-        n = parseInt(n);
-        const [dx, dy] = direction[op];
-        
-        const newPos = Array.from({length: n}, (_, step) => {
-            const newX = pos[0] + dx * (step + 1);
-            const newY = pos[1] + dy * (step + 1);
-            return [newX, newY];
-        });
+  for (const route of routes) {
+    const [op, n] = route.split(" ");
+    const [dr, dc] = dir[op];
+    const dist = Number(n);
+    let valid = true;
 
-        const isValid = newPos.every(([x, y]) => 
-            x >= 0 && x < h && y >= 0 && y < w && park[x][y] !== 'X'
-        );
+    for (let i = 1; i <= dist; i++) {
+      const nr = r + dr * i;
+      const nc = c + dc * i;
+      if (nr < 0 || nr >= H || nc < 0 || nc >= W || park[nr][nc] === "X") {
+        valid = false;
+        break;
+      }
+    }
 
-        return isValid ? newPos[n - 1] : pos;
-    };
+    if (valid) {
+      r += dr * dist;
+      c += dc * dist;
+    }
+  }
 
-    // 모든 명령을 처리하여 최종 위치 반환
-    return routes.reduce((pos, route) => {
-        const command = route.split(' ');
-        return move(pos, command);
-    }, startPos);
+  return [r, c];
 }
